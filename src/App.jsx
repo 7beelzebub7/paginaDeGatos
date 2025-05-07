13625e4d6b2a9f9487d9a1a214a6b8950d9ff3b6
7beelzebub7 <rc0870984@gmail.com>
2025-05-07 15:22:27 -0500

    corrijo las direcciones de las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
       <div id="razas" className="flex flex-col md:flex-row md:flex-wrap">
           <div className="bg-pink-200 p-7 md:basis-3/5 lg:basis-1/2 xl:basis-1/2 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/persa.webp" alt="gato persa" />
+            <img className="rounded-xl" src="./images/persa.webp" alt="gato persa" />
             </div>
             <p className="text-2xl font-bold">Persa</p>
               <p>El gato Persa es una raza de tamaño mediano reconocida por su largo pelaje sedoso y su cara aplanada. Tiene un temperamento tranquilo y afectuoso, ideal para hogares serenos. Su peso oscila entre los 3.5 y 7 kilogramos, y su esperanza de vida está entre los 12 y 17 años. Requiere cuidados constantes de su pelaje, incluyendo cepillado diario.
@@ -66,7 +66,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-2/5 lg:basis-1/2 xl:basis-1/2 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/siames.avif" alt="gato siames" />
+            <img className="rounded-xl" src="./images/siames.avif" alt="gato siames" />
             </div>
             <p className="text-2xl font-bold">Siamés</p>
               <p>El Siamés es un gato elegante y delgado, de pelaje claro con puntos oscuros en las extremidades y ojos azules muy expresivos. Es una raza sociable, vocal y muy cercana a sus dueños. Pesa entre 2.5 y 5.5 kilogramos y puede vivir entre 15 y 20 años. Es ideal para quienes buscan un gato activo y conversador.
@@ -74,7 +74,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-2/5 lg:basis-1/3 xl:basis-1/3 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/maineCoon.jpg" alt="gato maine coon" />
+            <img className="rounded-xl" src="./images/maineCoon.jpg" alt="gato maine coon" />
             </div>
             <p className="text-2xl font-bold">Maine Coon</p>
               <p>El Maine Coon es una de las razas de gatos más grandes del mundo. Tiene un cuerpo musculoso, pelaje largo y espeso, y una personalidad amigable y juguetona. Los machos pueden pesar entre 6 y 8 kilogramos, mientras que las hembras rondan los 4.5 a 6 kilogramos. Su esperanza de vida es de 12 a 15 años y necesita espacio y cepillado regular.
@@ -82,7 +82,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-3/5 lg:basis-1/3 xl:basis-1/3 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/bengali.webp" alt="gato bengalí" />
+            <img className="rounded-xl" src="./images/bengali.webp" alt="gato bengalí" />
             </div>
             <p className="text-2xl font-bold">Bengalí</p>
               <p>El gato Bengalí se caracteriza por su pelaje exótico, similar al de un leopardo, y su alta energía. Es un felino curioso, inteligente y muy activo, que disfruta de juegos y desafíos. Su peso va de los 4.5 a los 7 kilogramos, y puede vivir entre 12 y 16 años. Requiere estimulación constante para mantenerse feliz.
@@ -90,7 +90,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-3/5 lg:basis-1/3 xl:basis-1/3 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/esfinge.webp" alt="gato esfinge o sphynx" />
+            <img className="rounded-xl" src="./images/esfinge.webp" alt="gato esfinge o sphynx" />
             </div>
             <p className="text-2xl font-bold">Sphynx o Esfinge</p>
               <p>El Sphynx, o gato Esfinge, es conocido por su apariencia única sin pelo, piel arrugada y cuerpo cálido al tacto. Es un gato extrovertido, sociable y muy cariñoso. Pesa entre 3.5 y 7 kilogramos y su esperanza de vida está entre los 6 y 8 años. Necesita baños frecuentes para mantener su piel limpia y saludable.
@@ -98,7 +98,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-2/5 lg:basis-1/2 xl:basis-1/2 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/ragdoll.jpg" alt=" gato ragdoll" />
+            <img className="rounded-xl" src="./images/ragdoll.jpg" alt=" gato ragdoll" />
             </div>
             <p className="text-2xl font-bold">Ragdoll</p>           
               <p>El Ragdoll es un gato grande, de ojos azules intensos y pelaje semilargo. Se caracteriza por su docilidad y su tendencia a relajarse completamente cuando lo cargan. Los machos pueden pesar entre 6 y 9 kilogramos, y las hembras entre 4.5 y 7 kilogramos. Su esperanza de vida está entre los 10 y 15 años, y requiere cepillado regular.
@@ -106,7 +106,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-2/5 lg:basis-1/2 xl:basis-1/2 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/britanicoPCorto.jpg" alt="gato britanico de pelo corto" />
+            <img className="rounded-xl" src="./images/britanicoPCorto.jpg" alt="gato britanico de pelo corto" />
             </div>
             <p className="text-2xl font-bold">Britanico</p>
             <p>El Británico de Pelo Corto es un gato robusto y compacto, con pelaje denso y suave. Tiene un carácter calmado y reservado, pero es muy leal con su familia. Pesa entre 4 y 9 kilogramos y vive entre 12 y 17 años. Es propenso a ganar peso, por lo que necesita una dieta equilibrada y algo de ejercicio.
@@ -114,7 +114,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-3/5 lg:basis-1/3 xl:basis-1/3 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/scottishFold.jpg" alt="gato scottish fold" />
+            <img className="rounded-xl" src="./images/scottishFold.jpg" alt="gato scottish fold" />
             </div>
             <p className="text-2xl font-bold">Scottish Fold</p>
             <p>El Scottish Fold se distingue por sus orejas dobladas hacia adelante, que le dan una apariencia muy tierna. Es un gato de tamaño mediano, tranquilo y afectuoso. Su peso oscila entre 2.7 y 6 kilogramos y su esperanza de vida está entre los 9 y 12 años. Puede presentar problemas articulares, por lo que requiere revisiones veterinarias periódicas.
@@ -122,7 +122,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-3/5 lg:basis-1/3 xl:basis-1/3 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/gatoAzulRuso.avif" alt="gato azul ruso" />
+            <img className="rounded-xl" src="./images/gatoAzulRuso.avif" alt="gato azul ruso" />
             </div>
             <p className="text-2xl font-bold">Azul Ruso</p>
             <p>El Azul Ruso es un gato elegante de pelaje corto gris plateado y ojos verdes. Es reservado con los desconocidos pero muy apegado a sus dueños. Pesa entre 3.5 y 5.5 kilogramos y vive entre 10 y 15 años. Es ideal para hogares tranquilos y se adapta bien a vivir en interiores.
@@ -130,7 +130,7 @@ export default function App() {
           </div>
           <div className="bg-pink-200 p-7 md:basis-2/5 lg:basis-1/3 xl:basis-1/3 rounded-sm">
             <div className="flex justify-center">
-            <img className="rounded-xl" src="images/mestizo.jpg" alt="gato mestizo" />
+            <img className="rounded-xl" src="./images/mestizo.jpg" alt="gato mestizo" />
             </div>
             <p className="text-2xl font-bold">Mestizo</p>
             <p>El gato mestizo, también conocido como gato común, es el más frecuente en Colombia y en muchos otros países. Presenta una gran variedad de colores, patrones y personalidades. Su peso va de los 3 a los 5.5 kilogramos y puede vivir entre 12 y 18 años. Gracias a su diversidad genética, suele ser muy saludable y adaptable a distintos tipos de hogar.
